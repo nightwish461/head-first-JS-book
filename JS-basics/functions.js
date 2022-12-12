91f4981ce6a9b52ed58e73502841346ba31a1bc9
nightwigh461 <71612689+nightwish461@users.noreply.github.com>
2022-12-12 07:28:04 +0100

    Added more exercises on string and array operations

--- JS-basics/functions.js
+++ JS-basics/functions.js
@@ -85,4 +85,79 @@ console.log(
   printerErrorV2("gggguuuwbbbbbbhhhhhhhhhhuuuupppzzzzzzzz")
 );
 
-module.exports = { countVowels, countVowels2, printerErrorV2 };
+/*
+  Complete the solution so that it reverses the string passed into it.
+
+  'world'  =>  'dlrow'
+  'word'   =>  'drow'
+
+  Source: Codewars
+*/
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+console.log("reverseString ", reverseString("world"));
+
+/*
+  Given two integers a and b, which can be positive or negative, find the sum of all the integer
+  between and including them and return it. If the two numbers are equal return a or b.
+
+  Note: a and b are not ordered!
+
+  Examples (a, b) --> output (explanation)
+  (1, 0) --> 1 (1 + 0 = 1)
+  (1, 2) --> 3 (1 + 2 = 3)
+  (1, 3) --> 3 (1 + 2 + 3 = 6)
+  (0, 1) --> 1 (0 + 1 = 1)
+  (1, 1) --> 1 (1 since both are same)
+  (-1, 0) --> -1 (-1 + 0 = -1)
+  (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+  Source: Codewars
+*/
+function getSum(a, b) {
+  if (a > b) {
+    let tmp = a;
+    a = b;
+    b = tmp;
+  }
+  let sum = 0;
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+  return sum;
+}
+console.log("getSum V1: ", getSum(1, 3));
+console.log("getSum V1: ", getSum(-1, 3));
+console.log("getSum V1: ", getSum(0, -1));
+console.log("getSum V1: ", getSum(17, -453));
+
+/*
+  Given a non-empty array of integers, return the result of multiplying the values together in order. 
+  Example: [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+  Source: Codewars
+*/
+function growMultiply(x) {
+  let result = 1;
+  for (let i = 0; i < x.length; i++) {
+    result *= x[i];
+  }
+  return result;
+}
+console.log("growMultiply V1: ", growMultiply([1, 2, 3, 4]));
+
+// Improved version with reduce()
+function growMultiplyV2(x) {
+  return x.reduce(function (product, n) {
+    return product * n;
+  });
+}
+console.log("growMultiply reduce V2: ", growMultiplyV2([1, 2, 3, 4]));
+
+module.exports = {
+  countVowels,
+  countVowels2,
+  printerErrorV2,
+  reverseString,
+  getSum,
+  growMultiply,
+};
--- JS-basics/test/functions-test.js
+++ JS-basics/test/functions-test.js
@@ -29,4 +29,61 @@ describe("Function in JS tests", function () {
       expect(errorRate).to.equal("19/39");
     });
   });
+
+  describe("#reverseString", function () {
+    it("Reverse empty string", function () {
+      const strReversed = functionsJS.reverseString("");
+      expect(strReversed).to.equal("");
+    });
+    it("Reverse string world", function () {
+      const strReversed = functionsJS.reverseString("world");
+      expect(strReversed).to.equal("dlrow");
+    });
+    it("Reverse string 'Today is a good day'", function () {
+      const strReversed = functionsJS.reverseString("Today is a good day");
+      expect(strReversed).to.equal("yad doog a si yadoT");
+    });
+  });
+
+  describe("#getSum", function () {
+    it("Get sum between two given integers: 1, 0", function () {
+      const sum = functionsJS.getSum(1, 0);
+      expect(sum).to.equal(1);
+    });
+    it("Get sum between two given integers: 1, 2", function () {
+      const sum = functionsJS.getSum(1, 2);
+      expect(sum).to.equal(3);
+    });
+    it("Get sum between two given integers: 1, 3", function () {
+      const sum = functionsJS.getSum(1, 3);
+      expect(sum).to.equal(6);
+    });
+    it("Get sum between two given integers: 0, 1", function () {
+      const sum = functionsJS.getSum(0, 1);
+      expect(sum).to.equal(1);
+    });
+    it("Get sum between two given integers: -1, 0", function () {
+      const sum = functionsJS.getSum(-1, 0);
+      expect(sum).to.equal(-1);
+    });
+    it("Get sum between two given integers: 0, -2", function () {
+      const sum = functionsJS.getSum(0, -2);
+      expect(sum).to.equal(-3);
+    });
+    it("Get sum between two given integers: 17, -453", function () {
+      const sum = functionsJS.getSum(17, -453);
+      expect(sum).to.equal(-102678);
+    });
+  });
+
+  describe("#growMultiply", function () {
+    it("Multiply all integers in a given array: [1, 2, 3, 4]", function () {
+      const product = functionsJS.growMultiply([1, 2, 3, 4]);
+      expect(product).to.equal(24);
+    });
+    it("Multiply all integers in a given array: [1, 5, 2, 10]", function () {
+      const product = functionsJS.growMultiply([1, 5, 2, 10]);
+      expect(product).to.equal(100);
+    });
+  });
 });
